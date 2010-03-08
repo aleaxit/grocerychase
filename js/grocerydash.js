f08157d04eb8d026585159f12644e3f421e9827a
Alex Martelli <aleaxit@gmail.com>
2010-03-07 16:44:09 -0800

    More fixes, w3c validator down to 7 errors + 8 warnings (biggies coming...).

--- js/grocerydash.js
+++ js/grocerydash.js
@@ -1,10 +1,10 @@
 // Project settings
 
 var jQT = $.jQTouch({
-	icon: 'images/shoppingcart48.png',
-	startupScreen: 'images/food-pyramid.jpg',
-	addGlossToIcon: false,
-	statusBar: 'default'
+  icon: 'images/shoppingcart48.png',
+  startupScreen: 'images/food-pyramid.jpg',
+  addGlossToIcon: false,
+  statusBar: 'default'
 });
 
 var name = '';
@@ -18,87 +18,87 @@ $(function() {
 
 function pageIsLoaded()
 {
-	//$("#first-background");
-	//.bind("pageAnimationEnd");
-	//displayNoInput();
+  //$("#first-background");
+  //.bind("pageAnimationEnd");
+  //displayNoInput();
 }
 
 function displayNoInput()
 {
-	//alert("No input was provided.")	
+  //alert("No input was provided.") 
 }
 
 function cartWasTapped()
 {
-	//alert("Clicked!");
-	jQT.goTo("#selectPeriod", 'slide');
-	
-}	
+  //alert("Clicked!");
+  jQT.goTo("#selectPeriod", 'slide');
+  
+} 
 
 function cartWasTapped1()
 {
-	//alert("Clicked!");
-	jQT.goTo("#selectItems1", 'slide');
-	
+  //alert("Clicked!");
+  jQT.goTo("#selectItems1", 'slide');
+  
 }
 
 
 function cartWasTapped2()
 {
-	//alert("Clicked!");
-	jQT.goTo("#selectItems2", 'slide');
-	
+  //alert("Clicked!");
+  jQT.goTo("#selectItems2", 'slide');
+  
 }
 
 
 function cartWasTapped3()
 {
-	//alert("Clicked!");
-	jQT.goTo("#selectItems3", 'slide');
-	
+  //alert("Clicked!");
+  jQT.goTo("#selectItems3", 'slide');
+  
 }
 
 function cartWasTapped4()
 {
-	//alert("Clicked!");
-	jQT.goTo("#selectItems4", 'slide');
-	
+  //alert("Clicked!");
+  jQT.goTo("#checkout", 'slide');
+  
 }
 
 
 function milkWasTapped()
 {
-	$("#selectItems1").append('<div class="milkList" > <li><div class="milkType" id="whole" onclick="wholeWasClicked()">Whole</div><div class="milkType" id="2percent">2% Milk</div><div class="milkType" id="1percent">1% Milk</div></li> </div>');
-	
+  $("#selectItems1").append('<div class="milkList" > <li><div class="milkType" id="whole" onclick="wholeWasClicked()">Whole</div><div class="milkType" id="2percent">2% Milk</div><div class="milkType" id="1percent">1% Milk</div></li> </div>');
+  
 }
 
 function incrementMilk()
 {
-	var x = 0;
-	x = x + 1;
+  var x = 0;
+  x = x + 1;
 }
 
 function instructionsWasTapped()
 {
-	$("#firstBackground").append('<div class="instructions1" id="help1" onclick="hideInstructions()">These are the Instructions</div>');
+  $("#firstBackground").append('<div class="instructions1" id="help1" onclick="hideInstructions()">These are the Instructions</div>');
 }
 
 
 function nameWasTapped()
 {
-	$("#firstBackground").append('<div class="yourName" id="name1">Enter Your Name<input id="yourName1" type="text" name= "firstName"/><input type="submit" value="send" onclick="getName()"/></div>');
+  $("#firstBackground").append('<div class="yourName" id="name1">Enter Your Name<input id="yourName1" type="text" name= "firstName"/><input type="submit" value="send" onclick="getName()"/></div>');
 }
 
-	
+  
 function hideInstructions()
 {
-	$("#help1").fadeOut("slow");
-	
+  $("#help1").fadeOut("slow");
+  
 }
 
 function getName()
 {
- 	$("#name1").fadeOut("slow");
-	bigName = $("#yourName1").val();
-	alert(bigName);
+  $("#name1").fadeOut("slow");
+  bigName = $("#yourName1").val();
+  alert(bigName);
 }
