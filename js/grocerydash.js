fd3f8191a8ab7acc3b934a307c7298562b463e44
Alex Martelli <aleaxit@gmail.com>
2010-03-07 19:09:20 -0800

    Lots and LOTS more fixed, passed HTML5 validation, timer works, etc etc.
    Still needs CSS fixing (color names, font family, -webkit stuff, ...).

--- js/grocerydash.js
+++ js/grocerydash.js
@@ -35,34 +35,88 @@ function cartWasTapped()
   
 } 
 
-function cartWasTapped1()
-{
-  //alert("Clicked!");
-  jQT.goTo("#selectItems1", 'slide');
-  
+var nextaisle = 0;
+var itemsbyaisle = [
+  ['milk', 'cheese', 'yogurt'],
+  ['fruit', 'veggies', 'nuts'],
+  ['beef', 'chicken', 'fish', 'tofu']
+  ];
+var purchased = [];
+
+function buy(anitem) {
+  purchased.push(anitem);
+  $("#score").text(purchased.length);
 }
 
-
-function cartWasTapped2()
+function gotoaisle(whichaisle)
 {
-  //alert("Clicked!");
-  jQT.goTo("#selectItems2", 'slide');
-  
+  if (whichaisle >= itemsbyaisle.length) {
+    stopTimer();
+    $("#score1").text(purchased.length);
+    showTimer("#finalTime", $("#finalTime").text() + ' ');
+    $("#finalScore").text(
+        $("#finalScore").text() + ' ' + purchased.length
+        );
+    var finalist = '';
+    var i;
+    for(i=0; i<purchased.length; ++i) {
+      finalist = finalist + ' ' + purchased[i];
+    }
+    $("#finalList").text(finalist);
+    jQT.goTo("#checkout", 'slide');
+  } else {
+    nextaisle = whichaisle + 1;
+    $("#items").empty();
+    $("#score").text(purchased.length);
+    var i;
+    for(i=0; i<itemsbyaisle[whichaisle].length; ++i) {
+      var iname = itemsbyaisle[whichaisle][i];
+      $("#items").append(
+        '<button id="' + iname + '" class="item" ' +
+        'value="' + iname + '" onclick="buy(\'' +
+        iname + '\')">' + iname + '</button>'
+        );
+    }
+    if (whichaisle == 0) {
+      startTimer();
+      jQT.goTo("#aisle", 'slide');
+    }
+    // TODO: move the cart!
+  } 
 }
 
+var curTime = 0;
+var intervalId = 0;
 
-function cartWasTapped3()
-{
-  //alert("Clicked!");
-  jQT.goTo("#selectItems3", 'slide');
-  
+function showTimer(where, prefix) {
+  var seconds=Math.floor(curTime % 60) + "",
+      minutes=Math.floor((curTime/60) % 60) + "";
+  if (seconds.length === 1) {
+    seconds = "0" + seconds;
+  }
+  if (minutes.length === 1) {
+    minutes = "0" + minutes;
+  }
+  $(where).text(prefix + minutes + ":" + seconds);
 }
 
-function cartWasTapped4()
-{
-  //alert("Clicked!");
-  jQT.goTo("#checkout", 'slide');
-  
+function tick() {
+  curTime += 1;
+  showTimer("#timer", '');
+}
+
+function stopTimer() {
+  if (intervalId !== 0) {
+    clearInterval(intervalId);
+    intervalId = 0;
+  }
+}
+
+function startTimer() {
+  if (intervalId === 0) {
+    curTime = 0;
+    intervalId = setInterval(tick, 1000);
+  }
 }
 
 
@@ -72,24 +126,16 @@ function milkWasTapped()
   
 }
 
-function incrementMilk()
-{
-  var x = 0;
-  x = x + 1;
-}
-
 function instructionsWasTapped()
 {
   $("#firstBackground").append('<div class="instructions1" id="help1" onclick="hideInstructions()">These are the Instructions</div>');
 }
-
 
 function nameWasTapped()
 {
   $("#firstBackground").append('<div class="yourName" id="name1">Enter Your Name<input id="yourName1" type="text" name= "firstName"/><input type="submit" value="send" onclick="getName()"/></div>');
 }
 
-  
 function hideInstructions()
 {
   $("#help1").fadeOut("slow");
